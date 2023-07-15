d57179f4b3c70ff4c714a6548b9be2933670000d
Dimitri PERREAUX <dimitri.perreaux@gmail.com>
2023-07-15 22:06:02 +0200

    api/books(PUT): delete the previous picture if update a new file

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -41,10 +41,17 @@ exports.updateBookById = (req, res, next) => {
             if (book.userId != req.auth.userId) {
                 res.status(401).json( { message: 'non-autorisé'})
             } else {
+                if (req.file) {
+                    const filename = book.imageUrl.split('/images/')[1];
+                    fs.unlink(`images/${filename}`, () => {
+                        Book.updateOne( { _id: req.params.id}, { ...bookObject, _id: req.params.id})
+                        .then( () => res.status(200).json( {message: 'Livre modifié'}))
+                        .catch( error => res.status(401).json( {error} ))});
+                } else {
                 Book.updateOne( { _id: req.params.id}, { ...bookObject, _id: req.params.id})
                 .then( () => res.status(200).json( {message: 'Livre modifié'}))
                 .catch( error => res.status(401).json( {error} ))
-            }
+                }}
         })
 }
 
